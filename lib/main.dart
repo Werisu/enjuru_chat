203ee779b5a85404a8edeffbdef7e95c78a080a8
Werisu <wellysson35@gmail.com>
2020-01-02 22:12:21 -0300

    Layout

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,56 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 void main(){
-
   runApp(MyApp());
-
-  Firestore.instance.collection("teste").document("teste").setData({"teste" : "teste"});
 }
+
+final ThemeData kIOSTheme = ThemeData(
+  primarySwatch: Colors.orange,
+  primaryColor:  Colors.grey,
+  primaryColorBrightness: Brightness.light
+);
+
+final ThemeData kDefaultTheme = ThemeData(
+  primarySwatch: Colors.purple,
+  accentColor: Colors.orangeAccent[400],
+);
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: "Enjuru Chats",
+      debugShowCheckedModeBanner: false,
+      theme: Theme.of(context).platform == TargetPlatform.iOS ?
+          kIOSTheme : kDefaultTheme,
+      home: ChatScreen(),
+    );
   }
 }
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      bottom: false,
+      top: false,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Enjuru Chats"),
+          centerTitle: true,
+          elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+        ),
+        body: Column(
+
+        ),
+      ),
+    );
+  }
+}
+
 
